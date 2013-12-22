e37786671d62d92d4a0618f2a7c60e606e369c98
Carl Poole <carl@carlpoole.com>
2013-12-22 14:46:27 -0600

    Replaced if-else-if-else-if with switch for determining piece moves in Piece

--- src/chess/Piece.java
+++ src/chess/Piece.java
@@ -71,86 +71,91 @@ public class Piece {
 		ArrayList<Location> vals = new ArrayList<Location>();
 		Location currentLocation = loc;
 
-		if(this.type.getEType() == eType.PAWN){
-			if(this.team == 0){
-				if(currentLocation.getFile()+1 <= this.board.getWidth()-1
-                        && currentLocation.getRank()+1 <= this.board.getHeight()-1
-                        && (this.board.getSquares()[currentLocation.getFile()+1][currentLocation.getRank()+1].getOccupant() == null
-                        || this.board.getSquares()[currentLocation.getFile()+1][currentLocation.getRank()+1].getOccupant().team != 0))
-                            vals.add(this.board.getSquares()[currentLocation.getFile()+1][currentLocation.getRank()+1]);
+        switch(this.type.getEType())
+        {
+            //--- Possible PAWN moves ------------------------------------------------------
+            case PAWN:
+                if(this.team == 0){
+                    if(currentLocation.getFile()+1 <= this.board.getWidth()-1
+                            && currentLocation.getRank()+1 <= this.board.getHeight()-1
+                            && (this.board.getSquares()[currentLocation.getFile()+1][currentLocation.getRank()+1].getOccupant() == null
+                            || this.board.getSquares()[currentLocation.getFile()+1][currentLocation.getRank()+1].getOccupant().team != 0))
+                        vals.add(this.board.getSquares()[currentLocation.getFile()+1][currentLocation.getRank()+1]);
 
-				if(currentLocation.getFile()-1 >= 0 && currentLocation.getRank()+1 <= this.board.getHeight()-1
-                        && (this.board.getSquares()[currentLocation.getFile()-1][currentLocation.getRank()+1].getOccupant() == null
-                        || this.board.getSquares()[currentLocation.getFile()-1][currentLocation.getRank()+1].getOccupant().team != 0))
-                            vals.add(this.board.getSquares()[currentLocation.getFile()-1][currentLocation.getRank()+1]);
-			}
-			if(this.team == 1){
-				if(currentLocation.getFile()+1 <= this.board.getWidth()-1 && currentLocation.getRank()-1 >= 0
-                        && (this.board.getSquares()[currentLocation.getFile()+1][currentLocation.getRank()-1].getOccupant() == null
-                        || this.board.getSquares()[currentLocation.getFile()+1][currentLocation.getRank()-1].getOccupant().team != 1))
-                            vals.add(this.board.getSquares()[currentLocation.getFile()+1][currentLocation.getRank()-1]);
-				if(currentLocation.getFile()-1 >= 0 && currentLocation.getRank()-1 >= 0
-                        && (this.board.getSquares()[currentLocation.getFile()-1][currentLocation.getRank()-1].getOccupant() == null
-                        || this.board.getSquares()[currentLocation.getFile()-1][currentLocation.getRank()-1].getOccupant().team != 1))
-                            vals.add(this.board.getSquares()[currentLocation.getFile()-1][currentLocation.getRank()-1]);
-			}
-		}
-		else if(this.type.getEType() == eType.ROOK){
-
-			for(int i = currentLocation.getFile(); i <= board.getWidth()-1; i++){
-				if(this.board.getSquares()[i][currentLocation.getRank()].getOccupant() == null)
-                    vals.add(this.board.getSquares()[i][currentLocation.getRank()]);
-				else if(this.board.getSquares()[i][currentLocation.getRank()].getOccupant().team != this.team){
-					vals.add(this.board.getSquares()[i][currentLocation.getRank()]);
-					break;
-				}
-			}
-
-			for(int i = currentLocation.getFile(); i >= 0; i--){
-				if(this.board.getSquares()[i][currentLocation.getRank()].getOccupant() == null)
-                    vals.add(this.board.getSquares()[i][currentLocation.getRank()]);
-				else if(this.board.getSquares()[i][currentLocation.getRank()].getOccupant().team != this.team){
-					vals.add(this.board.getSquares()[i][currentLocation.getRank()]);
-					break;
-				}
-			}
-
-			for(int i = currentLocation.getRank(); i <= board.getHeight()-1; i++){
-				if(this.board.getSquares()[currentLocation.getFile()][i].getOccupant() == null)
-                    vals.add(this.board.getSquares()[currentLocation.getFile()][i]);
-				else if(this.board.getSquares()[currentLocation.getFile()][i].getOccupant().team != this.team){
-					vals.add(this.board.getSquares()[currentLocation.getFile()][i]);
-					break;
-				}
-			}
-
-			for(int i = currentLocation.getRank(); i >= 0; i--){
-				if(this.board.getSquares()[currentLocation.getFile()][i].getOccupant() == null)
-                    vals.add(this.board.getSquares()[currentLocation.getFile()][i]);
-				else if(this.board.getSquares()[currentLocation.getFile()][i].getOccupant().team != this.team){
-					vals.add(this.board.getSquares()[currentLocation.getFile()][i]);
-					break;
-				}
-			}
-		}
-        else if(this.type.getEType() == eType.BISHOP){
-            for(int i = 0; i <= Math.min(board.getWidth()-1-currentLocation.getFile(), board.getHeight()-1-currentLocation.getRank()); i++){
-                if(this.board.getSquares()[currentLocation.getFile()+i][currentLocation.getRank()+i].getOccupant() == null)
-                    vals.add(this.board.getSquares()[currentLocation.getFile()+i][currentLocation.getRank()+i]);
-                else if(this.board.getSquares()[currentLocation.getFile()+i][currentLocation.getRank()+i].getOccupant().team != this.team){
-                    vals.add(this.board.getSquares()[currentLocation.getFile()+i][currentLocation.getRank()+i]);
-                    break;
+                    if(currentLocation.getFile()-1 >= 0 && currentLocation.getRank()+1 <= this.board.getHeight()-1
+                            && (this.board.getSquares()[currentLocation.getFile()-1][currentLocation.getRank()+1].getOccupant() == null
+                            || this.board.getSquares()[currentLocation.getFile()-1][currentLocation.getRank()+1].getOccupant().team != 0))
+                        vals.add(this.board.getSquares()[currentLocation.getFile()-1][currentLocation.getRank()+1]);
                 }
-            }
-            for(int i = 0; i <= Math.min(board.getWidth()-1-currentLocation.getFile(), board.getHeight()-1-currentLocation.getRank()); i++){
-                if(this.board.getSquares()[currentLocation.getFile()+i][currentLocation.getRank()+i].getOccupant() == null)
-                    vals.add(this.board.getSquares()[currentLocation.getFile()+i][currentLocation.getRank()+i]);
-                else if(this.board.getSquares()[currentLocation.getFile()+i][currentLocation.getRank()+i].getOccupant().team != this.team){
-                    vals.add(this.board.getSquares()[currentLocation.getFile()+i][currentLocation.getRank()+i]);
-                    break;
+                if(this.team == 1){
+                    if(currentLocation.getFile()+1 <= this.board.getWidth()-1 && currentLocation.getRank()-1 >= 0
+                            && (this.board.getSquares()[currentLocation.getFile()+1][currentLocation.getRank()-1].getOccupant() == null
+                            || this.board.getSquares()[currentLocation.getFile()+1][currentLocation.getRank()-1].getOccupant().team != 1))
+                        vals.add(this.board.getSquares()[currentLocation.getFile()+1][currentLocation.getRank()-1]);
+                    if(currentLocation.getFile()-1 >= 0 && currentLocation.getRank()-1 >= 0
+                            && (this.board.getSquares()[currentLocation.getFile()-1][currentLocation.getRank()-1].getOccupant() == null
+                            || this.board.getSquares()[currentLocation.getFile()-1][currentLocation.getRank()-1].getOccupant().team != 1))
+                        vals.add(this.board.getSquares()[currentLocation.getFile()-1][currentLocation.getRank()-1]);
                 }
-            }
+                break;
+            //--- Possible ROOK moves ------------------------------------------------------
+            case ROOK:
+                for(int i = currentLocation.getFile(); i <= board.getWidth()-1; i++){
+                    if(this.board.getSquares()[i][currentLocation.getRank()].getOccupant() == null)
+                        vals.add(this.board.getSquares()[i][currentLocation.getRank()]);
+                    else if(this.board.getSquares()[i][currentLocation.getRank()].getOccupant().team != this.team){
+                        vals.add(this.board.getSquares()[i][currentLocation.getRank()]);
+                        break;
+                    }
+                }
+                for(int i = currentLocation.getFile(); i >= 0; i--){
+                    if(this.board.getSquares()[i][currentLocation.getRank()].getOccupant() == null)
+                        vals.add(this.board.getSquares()[i][currentLocation.getRank()]);
+                    else if(this.board.getSquares()[i][currentLocation.getRank()].getOccupant().team != this.team){
+                        vals.add(this.board.getSquares()[i][currentLocation.getRank()]);
+                        break;
+                    }
+                }
+                for(int i = currentLocation.getRank(); i <= board.getHeight()-1; i++){
+                    if(this.board.getSquares()[currentLocation.getFile()][i].getOccupant() == null)
+                        vals.add(this.board.getSquares()[currentLocation.getFile()][i]);
+                    else if(this.board.getSquares()[currentLocation.getFile()][i].getOccupant().team != this.team){
+                        vals.add(this.board.getSquares()[currentLocation.getFile()][i]);
+                        break;
+                    }
+                }
+                for(int i = currentLocation.getRank(); i >= 0; i--){
+                    if(this.board.getSquares()[currentLocation.getFile()][i].getOccupant() == null)
+                        vals.add(this.board.getSquares()[currentLocation.getFile()][i]);
+                    else if(this.board.getSquares()[currentLocation.getFile()][i].getOccupant().team != this.team){
+                        vals.add(this.board.getSquares()[currentLocation.getFile()][i]);
+                        break;
+                    }
+                }
+                break;
+            //--- Possible BISHOP moves ----------------------------------------------------
+            case BISHOP:
+                for(int i = 0; i <= Math.min(board.getWidth()-1-currentLocation.getFile(), board.getHeight()-1-currentLocation.getRank()); i++){
+                    if(this.board.getSquares()[currentLocation.getFile()+i][currentLocation.getRank()+i].getOccupant() == null)
+                        vals.add(this.board.getSquares()[currentLocation.getFile()+i][currentLocation.getRank()+i]);
+                    else if(this.board.getSquares()[currentLocation.getFile()+i][currentLocation.getRank()+i].getOccupant().team != this.team){
+                        vals.add(this.board.getSquares()[currentLocation.getFile()+i][currentLocation.getRank()+i]);
+                        break;
+                    }
+                }
+                for(int i = 0; i <= Math.min(board.getWidth()-1-currentLocation.getFile(), board.getHeight()-1-currentLocation.getRank()); i++){
+                    if(this.board.getSquares()[currentLocation.getFile()+i][currentLocation.getRank()+i].getOccupant() == null)
+                        vals.add(this.board.getSquares()[currentLocation.getFile()+i][currentLocation.getRank()+i]);
+                    else if(this.board.getSquares()[currentLocation.getFile()+i][currentLocation.getRank()+i].getOccupant().team != this.team){
+                        vals.add(this.board.getSquares()[currentLocation.getFile()+i][currentLocation.getRank()+i]);
+                        break;
+                    }
+                }
+                break;
+            default:
+                break;
         }
+
 		attackableSquares = vals;
 	}
 
